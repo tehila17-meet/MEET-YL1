42b8bb24c2b87f1f554610d1abe7495130773842
neta <netarvd@gmail.com>
2016-01-26 19:55:55 +0200

    haitham_good

---
+++ animal.py
@@ -0,0 +1,23 @@
+class Animal(object):
+	def __init__(self, sound, name, age):
+		self.sound=sound
+		self.name=name
+		self.age=age
+	def eat(self, food):
+		print(self.name + " is eating " +food)
+	def sleep(self, dream):
+		print("the " + self.age + " year old " + self.name + " is dreaming of " + self.sound)
+	
+class Bird(Animal):
+	def __init__(self, sound, name, age):
+		super().__init__(sound, name, age)
+	def fly(self):
+		print("the bird " + self.name +  " is flying")
+	
+	
+class Chicken(Animal):
+	def _init__(self, sound, name , age):
+		super().__init__(sound, name, age)
+	def fly(self):
+		print("oops the chicken " +self.name +" cant fly")	
+		
---
+++ filemeet.py
@@ -0,0 +1,20 @@
+from animal import*
+#animallist=[]
+#namelist=["dog","unicorn","dolphin"]
+#for i in range(0,50):
+#dog = Animal("food",namelist[random.randint(0,2)],"17")
+#animallist.append(dog)
+#for i in range(0,30):
+#cat = Animal("food",namelist[random.randint(0,2)],"5")
+#animallist.append(cat)
+#for i in animallist:
+#i.eat("bamba")
+#i.sleep("food")
+
+bird = Bird("Tweet", "barbara", 17)
+bird.fly()
+
+
+
+x = Chicken("cockadoolado" , "kuku",2)
+chicken.eat("bamba")
